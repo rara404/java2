1c292f23ebb060834e369b5396c3e6401c622c01
Raj Rana <raj.rana@fiserv.com>
2023-10-26 09:47:07 -0400

    completed java2 ex5b

--- src/main/java/ShoppingCart.java
+++ src/main/java/ShoppingCart.java
@@ -3,6 +3,11 @@ import java.util.ArrayList;
 public class ShoppingCart {
     private ArrayList<Items> shoppingCart;
     private float total = 0;
+    private double shippingCost = 0;
+
+    private double salesTax;
+
+
 
     public ShoppingCart() {
         this.shoppingCart = new ArrayList<>();
@@ -16,7 +21,21 @@ public class ShoppingCart {
         for(Items items: shoppingCart){
             total += (items.getPrice()*items.getQuantity());
         }
+        calculateShiping();
+        calculateSalesTax();
+        total += (this.salesTax + this.shippingCost);
     }
+
+    public void calculateShiping(){
+        if(this.total < 10)
+            this.shippingCost = 10;
+    }
+
+    public void calculateSalesTax(){
+            this.salesTax = 0.1 * this.total;
+    }
+
+
 
     public String getTotal(){
         return "$" + this.total;
